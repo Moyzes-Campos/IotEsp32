764b5ba6cb84a0f374fbb79aa98268be6ec76574
Moyzes Campos <moyzesstaron@gmail.com>
2025-03-16 08:28:09 -0300

    Atualizando o projeto

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+class Index(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Index, self).get_context_data(**kwargs)
+        return context
+
