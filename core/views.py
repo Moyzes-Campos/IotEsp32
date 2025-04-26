248b55f2d8151f86a148efa14ac66fceea90585f
Moyzes Campos <moyzesstaron@gmail.com>
2025-04-26 16:15:49 -0300

    Adicionando o decoretor na view

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 from .models import EstadoBotao
@@ -7,11 +8,11 @@ from .models import EstadoBotao
 from .forms import CreateEstadoForm
 
 
+@ensure_csrf_cookie
 class EstadoCreateView(CreateView):
     model = EstadoBotao
     template_name = 'core/add_estadobotao.html'
     form_class = CreateEstadoForm
-
 
 
 class IndexView(TemplateView):
