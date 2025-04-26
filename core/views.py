b5693b60e3bb7887286beb070146787dd65f0d7c
Moyzes Campos <moyzesstaron@gmail.com>
2025-04-26 16:40:32 -0300

    Segunda tentativa do method decorator

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.decorators.csrf import ensure_csrf_cookie
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
 from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 from .models import EstadoBotao
@@ -8,7 +9,7 @@ from .models import EstadoBotao
 from .forms import CreateEstadoForm
 
 
-@ensure_csrf_cookie
+@method_decorator(csrf_exempt, name='dispatch')
 class EstadoCreateView(CreateView):
     model = EstadoBotao
     template_name = 'core/add_estadobotao.html'
