21c6f7975d6f8c066eaa06976f871c92019a746b
Moyzes Campos <moyzesstaron@gmail.com>
2025-06-08 09:13:19 -0300

    Alterando o settings

--- IoT_Esp32/settings.py
+++ IoT_Esp32/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-skmi8a7c3@byz62wva!2nv&pg&=ej5-i+to^m9w3$3_0akmo=9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['44.203.255.70', '*']
 
